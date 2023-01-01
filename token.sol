1560ed02e265098f7cfcf5a39f1feb6089732a20
Constantino <Okonemmanuelmma@gmail.com>
2023-01-01 02:00:00 +0100

    transferFrom && approval functions

--- token.sol
+++ token.sol
@@ -1,21 +1,18 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.5.0;
 
-// abstract contract ERC20Basic {
-//     function totalSupply() external view returns (uint);
-//     function balanceOf(address who) external view returns (uint);
-//     function transfer(address to, uint value) external;
-//     event Transfer(address indexed from, address indexed to, uint value);
-// }
+interface ERC20 {
+    function totalSupply() external view returns (uint);
+    function balanceOf(address who) external view returns (uint);
+    function transfer(address to, uint value) external;
+    event Transfer(address indexed from, address indexed to, uint value);
+    function allowance(address owner, address spender) external view returns (uint);
+    function transferFrom(address from, address to, uint value) external;
+    function approve(address spender, uint value) external;
+    event Approval(address indexed owner, address indexed spender, uint value);
+}
 
-// abstract contract ERC20 is ERC20Basic {
-//     function allowance(address owner, address spender) external view returns (uint);
-//     function transferFrom(address from, address to, uint value) external;
-//     function approve(address spender, uint value) external;
-//     event Approval(address indexed owner, address indexed spender, uint value);
-// }
-
-contract token {
+abstract contract token is ERC20 {
     uint public _totalSupply;
     address public minter;
     string public name;
@@ -39,7 +36,7 @@ contract token {
 
     error InsuficientBalance(uint256 requested, uint256 available);
 
-    function transfer(address receiver, uint256 amount) public {
+    function transfer(address receiver, uint256 amount) public override{
         if (amount > balances[msg.sender]) {
             revert InsuficientBalance({
                 requested: amount,
@@ -52,17 +49,30 @@ contract token {
         emit Sent(msg.sender, receiver, amount);
     }
 
-    function totalSupply() public view returns(uint){
+    function transferFrom(address from, address to, uint amount) public override{
+         if (amount > balances[from]) {
+            revert InsuficientBalance({
+                requested: amount,
+                available: balances[from]
+            });
+        }
+        balances[from] -= amount;
+        balances[to] += amount;
+        emit Sent(msg.sender, receiver, amount);
+    }
+
+    function totalSupply() public view override returns(uint){
         return _totalSupply;
     }
 
-    function balanceOf(address who) public view returns(uint){
+    function balanceOf(address who) public view override returns(uint){
         return balances[who];
     }
 
-
-
-
-    // function approve()
+    function approve(address spender, uint amount) public override{
+        require(amount != 0);
+        allowed[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+    }
 
 }
