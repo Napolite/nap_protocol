53f113f74b5ffa90cc76f2f5af77738391b3ecbe
Constantino <Okonemmanuelmma@gmail.com>
2023-01-02 23:10:10 +0100

    created deploy contract

---
+++ script/index.js
@@ -0,0 +1,14 @@
+async function main() {
+  const napToken = await ethers.getContractFactory("Napolite");
+
+  // Start deployment, returning a promise that resolves to a contract object
+  const Napolite = await napToken.deploy("Hello World!");
+  console.log("Contract deployed to address:", Napolite.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
