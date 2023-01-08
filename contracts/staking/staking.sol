06222e21fac961f39f05b5c5091dbd278d0e9885
Constantino <Okonemmanuelmma@gmail.com>
2023-01-08 21:18:38 +0100

    added initial declarations for staking interface

---
+++ contracts/staking/staking.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.5.0
+
+mport "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/utils/SafeERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
+
+interface ERC20 {
+    function name() external view returns(string memory);
+    function symbol() external view returns(string memory);
+    function totalSupply() external view returns (uint);
+    function balanceOf(address who) external view returns (uint);
+    function transfer(address to, uint value) external;
+    event Transfer(address indexed from, address indexed to, uint value);
+    function allowance(address owner, address spender) external returns (uint);
+    function transferFrom(address from, address to, uint value) external;
+    function approve(address spender, uint value) external;
+    event Approval(address indexed owner, address indexed spender, uint value);
+}
+
+class Staking{
+
+}
